0442c9d812daf67e2f64118f40838f1916ea04d6
Brian McGehee <doc@tavian.com>
2015-08-05 13:21:45 -0700

    after

---
+++ src/hello_world.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub fn add_numbers(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn test_add_numbers() {
+    assert_eq!(3, add_numbers(1, 2));
+    assert_eq!(-7, add_numbers(5, -12));
+}
+
